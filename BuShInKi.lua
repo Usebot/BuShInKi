f6cb840ea9eb2dde2a3130e8fb9617ab25594790
Usebot <61378387+Usebot@users.noreply.github.com>
2020-05-14 12:24:21 +0300

    Add files via upload

--- BuShInKi.lua
+++ BuShInKi.lua
@@ -6137,6 +6137,20 @@ end
 end
 end,nil)   
 end
+if text and text:match('^وضع عدد الكانسر (%d+)$') and Owner(msg) then 
+local Num = text:match('^وضع عدد الكانسر (%d+)$')
+database:set(bot_id..'BuShInKi:Num:kansers'..msg.chat_id_,Num)
+send(msg.chat_id_, msg.id_, '\n⌔︙تم وضع عدد حروف الاسم {'..Num..'} حرف')
+if text == "فتح الكانسر" and Owner(msg) then 
+database:set(bot_id.."BuShInKi:Num:kansers"..msg.chat_id_) 
+send(msg.chat_id_, msg.id_,"☑︙تم تفعيل ترحيب المجموعه") 
+return false  
+end
+if text == "قفل الكانسر" and Owner(msg) then 
+database:del(bot_id.."BuShInKi:Num:kansers"..msg.chat_id_,true) 
+send(msg.chat_id_, msg.id_,"☑︙تم تعطيل ترحيب المجموعه") 
+return false  
+end
 if text == 'تفعيل ضافني' and Owner(msg) then   
 database:del(bot_id..'BuShInKi:Lock:Added:Me'..msg.chat_id_)  
 send(msg.chat_id_, msg.id_,'☑┇تم تفعيل امر منو ضافني') 
@@ -7443,6 +7457,20 @@ end
 if msg.sender_user_id_ and Muted_Groups(msg.chat_id_,msg.sender_user_id_) then 
 DeleteMessage(msg.chat_id_, {[0] = msg.id_})  
 return false  
+end
+if msg.content_.ID == "MessageChatJoinByLink" then
+if redis:get(bot_id.."BuShInKi:lock:kanser"..msg.chat_id_) then
+tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data) 
+local last_ = data.last_name_ or ''
+local first_ = data.first_name_ or ''
+local taha = (first_..''..last_)
+local Num = (database:get(bot_id..'BuShInKi:Num:kansers'..msg.chat_id_) or 25)
+if string.len(taha) > tonumber(Num) then
+send(msg.chat_id_, msg.id_,'\n⌔︙الكانسر مقفول يرجى زغرفه اسمك اولاً\n ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉  ┉ ┉ ┉ ┉\n⌔︙ اضغط هنا لزغرفه اسمك.')
+https.request("https://api.telegram.org/bot"..token.."/restrictChatMember?chat_id="..msg.chat_id_.."&user_id="..msg.sender_user_id_)
+end
+end,nil)   
+return false
 end
 --------------------------------------------------------------------------------------------------------------
 if tonumber(msg.sender_user_id_) ~= tonumber(bot_id) then  
