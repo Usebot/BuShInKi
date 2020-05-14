61a843df8183a675e8aa1ff7814190800b5c285e
Usebot <61378387+Usebot@users.noreply.github.com>
2020-05-14 12:45:03 +0300

    Add files via upload

--- BuShInKi.lua
+++ BuShInKi.lua
@@ -7595,20 +7595,6 @@ return false
 end
 end
 end
-if msg.content_.ID == "MessageChatJoinByLink" then
-if redis:get(bot_id.."BuShInKi:lock:kanser"..msg.chat_id_) then
-tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data) 
-local last_ = data.last_name_ or ''
-local first_ = data.first_name_ or ''
-local taha = (first_..''..last_)
-local Num = (database:get(bot_id..'BuShInKi:Num:kansers'..msg.chat_id_) or 25)
-if string.len(taha) > tonumber(Num) then
-send(msg.chat_id_, msg.id_,'⌔︙الكانسر مقفول يرجى زغرفه اسمك اوللا')
-https.request("https://api.telegram.org/bot"..token.."/restrictChatMember?chat_id="..msg.chat_id_.."&user_id="..msg.sender_user_id_)
-end
-end,nil)   
-return false
-end
 ------------------------------------------------------------------------
 if text and database:get(bot_id.."BuShInKi:Del:Cmd:Group"..msg.chat_id_..":"..msg.sender_user_id_) == "true" then
 local NewCmmd = database:get(bot_id.."BuShInKi:Set:Cmd:Group:New1"..msg.chat_id_..":"..text)
